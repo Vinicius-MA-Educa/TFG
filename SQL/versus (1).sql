9a0f3b715548014dc9981a2f52a15dbd5464a11e
Vinicius-MA-Educa <nicholas.vinalm@educa.jcyl.es>
2025-05-26 23:13:50 +0200

    Add files via upload

---
+++ SQL/versus (1).sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-05-2025 a las 23:13:24
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tfg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `versus`
+--
+
+CREATE TABLE `versus` (
+  `Nombre1` varchar(255) NOT NULL,
+  `Nombre2` varchar(255) NOT NULL,
+  `Puntos1` int(11) NOT NULL,
+  `Puntos2` int(11) NOT NULL,
+  `Turno` varchar(255) NOT NULL,
+  `Juego` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `versus`
+--
+ALTER TABLE `versus`
+  ADD PRIMARY KEY (`Nombre1`,`Nombre2`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
